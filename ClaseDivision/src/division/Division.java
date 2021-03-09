b2d3977dda943f5658aa443fe847c89f7da5ce2f
jorge <jorgea9@yahoo.es>
2021-03-09 09:09:24 +0100

    Añadimos clase division

---
+++ ClaseDivision/src/division/Division.java
@@ -0,0 +1,28 @@
+package division;
+
+public class Division {
+	
+	private static double division2Reales(double num1, double num2) {
+		double division;
+		division = num1 / num2;
+		return division;
+	}
+	
+	private static double division2Enteros(int num1, int num2) {
+		double division;
+		division = num1 / num2;
+		return division;
+	}
+	
+	private static double inversoNumero(double num1) {
+		double inverso;
+		inverso = 1 / num1;
+		return inverso;
+	}
+	
+	private static double raizCuadradaNumero(double num1) {
+		double raiz;
+		raiz = Math.sqrt(num1);
+		return raiz;
+	}
+}
